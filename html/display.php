e03775e1e1c2bb1fb3850baf58ebe2e39dd5ffbf
root <root@instance-strongbox.c.strongbox-220010.internal>
2018-11-17 23:51:51 +0000

    Cleaned up code and added explanations for functionality

--- html/display.php
+++ html/display.php
@@ -1,17 +1,23 @@
 <?php
+	//prevents users from accessing page directly
+	session_start();
+	if(!isset($_SESSION['logged']))
+	{
+		echo('<script>window.location="login.php"</script>');
+	}
 	//connect to online database
 	$conn = new PDO("mysql:host=35.205.202.112;dbname=Users","root","mtD{];ttcY^{9@>`");
-	//allows for uploaded files to be displayed
+	//allows for uploaded files to be downloaded
 	$id = isset($_GET['id'])? $_GET['id'] : "";
 	$files = $conn->prepare("select * from filestorage where f_id=?");
 	$files->bindParam(1,$id);
 	$files->execute();
+	//places file contents of single row for dowload
 	$row = $files->fetch();
-	//var_dump($row);
-	$key_password = "password";
 	$iv = $row['file_iv'];
 	$key = $row['file_key'];
 	$method = 'aes-256-cbc';
 	$file = openssl_decrypt($row['file_data'], $method, $key, OPENSSL_RAW_DATA, $iv);
+	//prints out entire contents
 	echo $file;
 ?>
--- html/home.php
+++ html/home.php
@@ -11,19 +11,20 @@ $conn = new PDO("mysql:host=35.205.202.112;dbname=Users","root","mtD{];ttcY^{9@>
 //if upload button clicked with file attached(add condition to check for this) attempts to upload to db
 if(isset($_POST['upload']))
 {
+	//passing file attributes from form
 	$name = $_FILES['file_upload']['name'];
 	$type = $_FILES['file_upload']['type'];
 	$file_data = $_FILES['file_upload']['tmp_name'];
 	$file_contents = file_get_contents($file_data);
-
 	//file encryption process
-	$key_password = $_FILES['key_encryption'];//read in password for key generation
+	$key_password = $_GET['key_encryption'];//read in password for key generation
 	$method = 'aes-256-cbc';//choose method of encryption; AES 256-bit
 	$key = substr(hash('sha256', $key_password, true), 0, 32);//generate key for encryption using given password
-	$iv_length = openssl_cipher_iv_length($method);
-	$iv = openssl_random_pseudo_bytes($iv_length);
+	$iv_length = openssl_cipher_iv_length($method);//decides initialization vector length
+	$iv = openssl_random_pseudo_bytes($iv_length);//generates an initialization vector for every encrypted file to prevent ciphertext duplicates
 	$data = openssl_encrypt($file_contents, $method, $key, OPENSSL_RAW_DATA, $iv);
-	//$data = openssl_decrypt(base64_decode($data), $method, $key, OPENSSL_RAW_DATA, $iv);
+	//$data = openssl_decrypt(base64_decode($data), $method, $key, OPENSSL_RAW_DATA, $iv);//just quick way of checking if file is encrypted/decrypted correctly
+	//inserting encrypted file values into sql database
 	$upload_file = $conn->prepare("insert into filestorage(file_name, file_type, file_data, file_iv, file_key) values(?,?,?,?,?)");
 	$upload_file->bindParam(1,$name);
 	$upload_file->bindParam(2,$type);
@@ -38,40 +39,27 @@ if(isset($_POST['upload']))
 	}
 }
 ?>
-    <form method="post" enctype="multipart/form-data">
+	//form used to require file for upload and password which will be used to generate a key fro encryption/decryption
+	<form method="post" enctype="multipart/form-data">
         <div>
         <input type="file" name="file_upload" required/>
 	<input type="text" name="key_encryption" required>
         <button name="upload">Upload</button>
         </div>
-    </form>
-    <p></p>
-    <?php
+	</form>
+	<br>
+	<?php
 	//selects all files stored on the database and prints their contents
 	$files = $conn->prepare("select * from filestorage");
 	$files->execute();
+	//creates hyperlink for downloading files stored on database which are decrypted using stored key (will be changed to localised method in future)
 	while($row = $files->fetch())
 	{
-		/*$key_password = "password";
-		$iv = $row['iv'];
-		$method = 'aes-256-cbc';
-		$key = substr(hash('sha256', $key_password, true), 0, 32);
-		$row['file_data'] = openssl_decrypt(base64_decode($row['file_data']), $method, $key, OPENSSL_RAW_DATA, $iv);*/
 		echo "<div>";
 		echo "<li><a href='display.php?id=".$row['f_id']."' target='_blank' download=".$row['file_name'].">".$row['file_name']."</a></li>";
 		echo "</div>";
 	}
-	/*$plaintext="Mikasa is best girl";
-	$password = 'password';
-	$method = 'aes-256-cbc';
-	$key = substr(hash('sha256', $password, true), 0, 32);
-	$iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
-        $encrypted = base64_encode(openssl_encrypt($plaintext, $method, $key, OPENSSL_RAW_DATA, $iv));
-	echo $encrypted;?><br><?php
-	$decrypted = openssl_decrypt(base64_decode($encrypted), $method, $key, OPENSSL_RAW_DATA, $iv);
-	echo $decrypted;*/
     ?>
-    </ol>
     <br>
     <a href="logout.php">Logout</a>
 </body>
